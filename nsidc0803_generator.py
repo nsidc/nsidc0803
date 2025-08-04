675cb5e481a0ee189a040ef3c14f1230dc9f3e1d
Robyn Marowitz <robyn.marowitz@colorado.edu>
2025-08-04 16:32:14 -0600

    fix longitude

--- nsidc0803_generator.py
+++ nsidc0803_generator.py
@@ -30,7 +30,7 @@ def get_grid_params():
             "ydim": 448,
             "crs_long_name": "NSIDC Sea Ice Polar Stereographic North",
             "straight_vertical_longitude_from_pole": -45.0,
-            "longitude_of_projection_origin": 0.0,
+            "longitude_of_projection_origin": -45.0,
             "latitude_of_projection_origin": 90.0,
             "latitude_of_standard_parallel": 70.0,
             "GeoTransform": "-3850000 25000 0 5850000 0 -25000 ",
