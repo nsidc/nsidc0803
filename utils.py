c30af550d54a2f92f1be5e0ed508429f481902cf
Robyn Marowitz <robyn.marowitz@colorado.edu>
2025-08-06 17:47:01 -0600

    temporary output

--- utils.py
+++ utils.py
@@ -12,7 +12,8 @@ import numpy as np
 import pyproj
 from netCDF4 import Dataset, date2num
 
-INPUT_DIR = "/disks/sidads_staging/DATASETS/nsidc0740_AS2_nrt_nasateam_seaice_v1/"
+# INPUT_DIR = "/disks/sidads_staging/DATASETS/nsidc0740_AS2_nrt_nasateam_seaice_v1/"
+INPUT_DIR = "/share/apps/nsidc0803/"
 OUTPUT_DIR = "/disks/sidads_staging/DATASETS/nsidc0803_daily_a2_seaice_conc/"
 
 
